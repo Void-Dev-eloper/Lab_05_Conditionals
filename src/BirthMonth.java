dd5ace8992ce44ec07060cdc45471270c475e247
ZN-Zz <badhikari1211@gmail.com>
2024-02-02 09:33:39 -0500

    BirthMonth

---
+++ src/BirthMonth.java
@@ -0,0 +1,15 @@
+public class BirthMonth {
+    public static void main (String[] args) {
+        /*
+        A program that asks the user to enter their birth month (integer 1 – 12 inclusive). If the user enters a value in range, the program echoes the input (“Your birth month is: N”) If the value is not in the range it outputs an error msg (“You entered an incorrect month value: N”).   Here N should be the value they entered.
+
+        Test runs:   (insert the output widow copies here for the test runs
+
+        - valid input in range 1 - 12
+
+        - invalid input out of range
+        */
+
+
+    }
+}
