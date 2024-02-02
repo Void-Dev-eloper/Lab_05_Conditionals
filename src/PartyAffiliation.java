d67cf58233aa5f51a045cd65dba5f0ba7d523134
ZN-Zz <badhikari1211@gmail.com>
2024-02-02 10:34:54 -0500

    PartyAffiliation Completed

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class PartyAffiliation {
     public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is your party affiliation (Democrat = D, Republican = R, Independent = I): ");
+        String userInput = in.next();
+        if (userInput.equals("D")){
+            System.out.println("You get a Democratic Donkey.");
+        } else if (userInput.equals("R")) {
+            System.out.println("You get a Republican Elephant.");
+        } else if (userInput.equals("I")) {
+            System.out.println("You get a Independent Person.");
+        }else {
+            System.out.println("Other");
+        }
 
     }
 }
