a6c28e6aa8697aa6fddb6d8f26f8d8edb56daa32
ZN-Zz <141598174+ZN-Zz@users.noreply.github.com>
2024-02-05 11:02:55 -0500

    NumCompare Still not finished From line: 19 - 20: fix

--- src/NumCompare.java
+++ src/NumCompare.java
@@ -1,4 +1,3 @@
-import java.util.Objects;
 import java.util.Scanner;
 public class NumCompare {
     public static void main (String[] args){
@@ -8,24 +7,24 @@ It indicates if they are equal or if they are not indicates the one that is less
 This program should be bullet-proofed. If either input is not a number, the program should indicate that the user should try again and terminate.
          */
         Scanner in = new Scanner(System.in);
-        int num1;
-        int num2;
+        Object num1;
+        Object num2;
 
         System.out.println("Enter the first number: ");
         num1 = in.nextInt();
 
-//        String type = (Object)num1);
-
         System.out.println("Enter the second number: ");
         num2 = in.nextInt();
-        System.out.println(in.hasNextInt());
 
-        if (!in.hasNextInt()) {
+        if (!(num1.getClass().getSimpleName()).equals("Integer") || !(num2.getClass().getSimpleName()).equals("Integer")) {
             System.out.println("Please Try Again: Either Number 1" + num1 + " is not a number/whole number or its Number 2: " + num2);
         }else {
-            if (num1 < num2) {
+            int compareNum1 = ((Integer) num1).intValue();
+            int compareNum2 = ((Integer) num2).intValue();
+
+            if (compareNum1 < compareNum2) {
                 System.out.println("Number: " + num1 + " is less than Number: " + num2);
-            }else if (num1 > num2) {
+            }else if (compareNum1 > compareNum2) {
                 System.out.println("Number: " + num1 + " is greater than Number: " + num2);
             }else {
                 System.out.println("Number: " + num1 + " is equal to Number: " + num2);
